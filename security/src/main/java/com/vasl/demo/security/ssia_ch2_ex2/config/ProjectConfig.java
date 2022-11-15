20eddcde1096c6972b78c7637a319a8766df8668
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-11-15 18:30:55 +0330

    Add Password Encoder

--- security/src/main/java/com/vasl/demo/security/ssia_ch2_ex2/config/ProjectConfig.java
+++ security/src/main/java/com/vasl/demo/security/ssia_ch2_ex2/config/ProjectConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -21,5 +23,10 @@ public class ProjectConfig {
         userDetailService.createUser(user);
 
         return userDetailService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
