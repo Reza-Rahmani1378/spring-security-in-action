9abdaaeddf530791fb3a5cd442a1ddc2d7f2556f
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-11-15 18:24:18 +0330

    Create UserDetailService For manage Users

---
+++ security/src/main/java/com/vasl/demo/security/ssia_ch2_ex2/config/ProjectConfig.java
@@ -0,0 +1,25 @@
+package com.vasl.demo.security.ssia_ch2_ex2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailService =
+                new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailService.createUser(user);
+
+        return userDetailService;
+    }
+}
