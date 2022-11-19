83d662ad26bba2651b9ff9faec6a5de803863a3b
Reza <reza6273757@gmail.com>
2022-11-19 17:37:20 +0330

    Add configure method

--- security/src/main/java/com/vasl/demo/security/ssia_ch2_ex2/config/ProjectConfig.java
+++ security/src/main/java/com/vasl/demo/security/ssia_ch2_ex2/config/ProjectConfig.java
@@ -2,6 +2,10 @@ package com.vasl.demo.security.ssia_ch2_ex2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -29,4 +33,11 @@ public class ProjectConfig {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+
 }
