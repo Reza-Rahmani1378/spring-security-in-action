79674a2808c43eaa0979bda409fc2de82126321d
Reza <reza6273757@gmail.com>
2022-11-23 11:17:42 +0330

    Refactor

--- security/src/main/java/com/vasl/demo/security/ssia_ch2_ex5/config/CustomAuthenticationProvider.java
+++ security/src/main/java/com/vasl/demo/security/ssia_ch2_ex5/config/CustomAuthenticationProvider.java
@@ -5,10 +5,12 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
